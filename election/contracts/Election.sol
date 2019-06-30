05a560c992153a1af520dd4c3fcb1e13376faa34
tommy3531 <thomas.marler@hotmail.com>
2019-06-30 10:35:39 -0500

    Added tests to check for candidate and attributes are correct

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -1,11 +1,30 @@
 pragma solidity ^0.5.8;
 
 contract Election {
-	// State variable
-	string public candidate;
+	// Modle a candidata
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
 
-	// Constructor
+	// Store candidate
+	// Fetch candidate
+	mapping(uint => Candidate) public candidates;
+
+	// Count candidate
+	uint public candidatesCount;
+
+
 	constructor() public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string memory _name) private {
+		// Increase candidate count
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
+
