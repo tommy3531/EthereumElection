7eec22ae3d2be4b14ab791cc5684d3737412ae11
tommy3531 <thomas.marler@hotmail.com>
2019-06-30 15:02:17 -0500

    Keep track of votes and, no double voting

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -8,6 +8,9 @@ contract Election {
 		uint voteCount;
 	}
 
+	// Store accounts that have voted
+	mapping(address => bool) public voters;
+
 	// Store candidate
 	// Fetch candidate
 	mapping(uint => Candidate) public candidates;
@@ -25,6 +28,20 @@ contract Election {
 		// Increase candidate count
 		candidatesCount++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote (uint _candidateId) public {
+		// require that they havent voted before
+		require(!voters[msg.sender]);
+
+		// require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+		
+		// record that voter has voted
+		voters[msg.sender] = true;
+
+		// update candidate vote count
+		candidates[_candidateId].voteCount ++;
 	}
 }
 
